c5e1990ceeebb250cdfd30e67dc5063d722d8df8
philip-dman <hanzphilip.manaog@email.lcup.edu.ph>
2025-09-23 18:58:51 +0800

    Implement Forms component with input and submit functionality

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,5 +1,30 @@
-const Forms = () => {
-  return <div>Forms</div>;
-};
+import { useState } from "react";
 
-export default Forms;
+export default function Forms() {
+  const [text, setText] = useState("");
+  const [a, setA] = useState("");
+
+  return (
+    <div className="flex flex-col w-full h-full items-center justify-center">
+      Forms
+      <input
+        type="text"
+        placeholder="Type here"
+        className="input"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <p className="mt-4">{a}</p>
+      <button
+        className="btn mt-4"
+        onClick={() => {
+          console.log(text);
+          setA(text);
+          setText("");
+        }}
+      >
+        submit
+      </button>
+    </div>
+  );
+}
