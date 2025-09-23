1573bb1478132825487149d689dc34da74f2db7a
John Christian D. Rosuelo <152044019+AetherKnowledge@users.noreply.github.com>
2025-09-23 20:37:50 +0800

    Simple Example

--- src/components/UseState/UseState.tsx
+++ src/components/UseState/UseState.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 const UseState = () => {
-  return <div>UseState</div>;
+  let count = 0;
+  const [countWithState, setCountWithState] = useState(0);
+
+  function handleClick() {
+    count++;
+    setCountWithState(countWithState + 1);
+
+    console.log("Without State: ", count);
+    console.log("With State: ", countWithState);
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4">
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={() => handleClick()}
+      >
+        Add Count
+      </button>
+      <p>Without State: {count}</p>
+      <p>With State: {countWithState}</p>
+    </div>
+  );
 };
 
 export default UseState;
